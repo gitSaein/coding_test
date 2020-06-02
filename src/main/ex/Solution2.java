dcdbf658197ce0f8df890d0cab1d93e7bef6d710
saeinlee <>
2020-06-02 16:27:01 +0900

    numberFormat

--- src/main/ex/Solution2.java
+++ src/main/ex/Solution2.java
@@ -1,5 +1,21 @@
 package main.ex;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Solution2 {
 
+    public static void main(String[] args) {
+        Locale locale = new Locale("en", "IN");
+
+        NumberFormat numberFormat = NumberFormat.getInstance(locale);
+        System.out.println("getInstance: " + numberFormat.format(100000000.99));
+
+        NumberFormat numberFormat2 = NumberFormat.getCurrencyInstance(locale);
+        System.out.println("getCurrencyInstance: " + numberFormat2.format(100.999));
+
+        NumberFormat percentageFormat = NumberFormat.getPercentInstance(locale);
+        System.out.println("getPercentInstance: " + percentageFormat.format(2000.399));
+
+    }
 }
